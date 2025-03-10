cd4f5ed6e988c4b2b9ca42971e637d5f2e56e916
gpintus <gpintus@travix.com>
2025-03-10 14:58:35 +0000

    test upgrade

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/alecthomas/kingpin"
 	foundation "github.com/estafette/estafette-foundation"
 	"github.com/rs/zerolog/log"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 var (
